6969f095db597dbdfa2d929768c35b441f21d923
Thirupathi Rao <162027483+thirupathi4@users.noreply.github.com>
2025-11-07 11:14:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 //your JS code here. If required.
+    
+ document.getElementById("userForm").addEventListener("submit", function(event) {
+      event.preventDefault(); // Prevent form from refreshing
+
+      let name = document.getElementById("name").value.trim();
+      let age = document.getElementById("age").value.trim();
+
+      // Validate input
+      if (name === "" || age === "") {
+        alert("Please enter valid details.");
+        
+      }
+	 let checkPromise=new Promise((resolve,reject)=>{
+		 if(age>18){
+			 setTimeout(resolve(alert("Welcome, . You can vote.")),4000)
+		 }
+		 else if(age<18){
+			 setTimeout(reject(alert("Oh sorry . You aren't old enough.")),4000)
+		 }
+		 }
+	 })
+	 
+ 
